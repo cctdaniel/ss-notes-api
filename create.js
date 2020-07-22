0ea140f2eaaf4e8aa1a6ae76e3bb22e39e63530f
Daniel Chew <cctdaniel@outlook.com>
2020-07-22 14:21:35 +0800

    Remove eslint config file

--- create.js
+++ create.js
@@ -1,9 +1,9 @@
-import * as uuid from "uuid";
-import handler from "./libs/handler-lib";
-import dynamoDb from "./libs/dynamodb-lib";
+import * as uuid from "uuid"
+import dynamoDb from "./libs/dynamodb-lib"
+import handler from "./libs/handler-lib"
 
 export const main = handler(async (event, context) => {
-  const data = JSON.parse(event.body);
+  const data = JSON.parse(event.body)
   const params = {
     TableName: process.env.tableName,
     // 'Item' contains the attributes of the item to be created
@@ -19,11 +19,11 @@ export const main = handler(async (event, context) => {
       noteId: uuid.v1(),
       content: data.content,
       attachment: data.attachment,
-      createdAt: Date.now()
-    }
-  };
+      createdAt: Date.now(),
+    },
+  }
 
-  await dynamoDb.put(params);
+  await dynamoDb.put(params)
 
-  return params.Item;
-});
+  return params.Item
+})
